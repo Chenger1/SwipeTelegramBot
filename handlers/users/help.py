c29ff761a0d4c462fd4f3d50a991f37d1ceac072
Chenger1 <exs2199@gmail.com>
2021-08-11 16:20:10 +0300

    Update help message

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -3,11 +3,14 @@ from aiogram.dispatcher.filters.builtin import CommandHelp
 
 from loader import dp
 
+from middlewares import _
+
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
-    text = ("Список команд: ",
-            "/start - Начать диалог",
-            "/help - Получить справку")
+    text = _("Список команд: \n" +
+             "/start - Начать диалог\n" +
+             "/help - Получить справку\n" +
+             'SwipeTelegramBot поможет вам получить информацию об актуальных предложениях аренды')
     
-    await message.answer("\n".join(text))
+    await message.answer(text)
